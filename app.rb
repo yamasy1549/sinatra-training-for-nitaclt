4545e1ed7d8f28057811d7bd95623fe62afee6b5
yamasy1549 <sanae@yamasy.info>
2017-07-12 15:29:50 +0900

    テンプレートを使って表示してみよう

--- app.rb
+++ app.rb
@@ -4,6 +4,6 @@ require 'sinatra/activerecord'
 require './models/card.rb'
 
 get '/cards' do
-  card = Card.first
-  "#{card.word}: #{card.description}"
+  cards = Card.all
+  erb :index, locals: { cards: cards }
 end
